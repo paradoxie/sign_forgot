36b0f6e33c869c5c4eeca6ad3ca1508bddd019e1
paradox <paradoxieix@gmail.com>
2024-11-27 17:59:10 +0800

    更改logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -57,14 +57,14 @@ const Header = () => {
                 } `}
               >
                 <Image
-                  src='/images/logo/6677-logo-light.png'
+                  src='/images/logo/logo-2.svg'
                   alt='logo'
                   width={140}
                   height={30}
                   className='w-full dark:hidden'
                 />
                 <Image
-                  src='/images/logo/6677-logo.png'
+                  src='/images/logo/logo-2.svg'
                   alt='logo'
                   width={140}
                   height={30}
--- src/components/Header/menuData.tsx
+++ src/components/Header/menuData.tsx
@@ -15,16 +15,16 @@ const menuData: Menu[] = [
   },
   {
     id: 33,
-    title: 'Blog',
+    title: '博客',
     path: '/blog',
     newTab: false,
   },
-  // {
-  //   id: 3,
-  //   title: 'Support',
-  //   path: '/contact',
-  //   newTab: false,
-  // },
+  {
+    id: 3,
+    title: '联系我们',
+    path: '/contact',
+    newTab: false,
+  },
   // {
   //   id: 4,
   //   title: "Pages",
